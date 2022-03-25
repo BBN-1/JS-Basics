eb7fc6136ca89f1c935a673d5fdd6b29d6accce8
Martin Neshev <100307424+BBN-1@users.noreply.github.com>
2022-03-25 10:56:49 +0200

    Create Number Pyramid.js

---
+++ Nested Loops - Exercise/Number Pyramid.js
@@ -0,0 +1,30 @@
+function solve(input) {
+
+    let targetNum = Number(input[0]);
+    let currentNum = 0;
+    let isEqual = false;
+
+
+    for (let rows = 1; rows <= targetNum; rows++) {
+        let printLine = ``;
+
+        for (let columns = 1; columns <= rows; columns++) {
+            currentNum++;
+            printLine += currentNum + ` `;
+            if (currentNum === targetNum) {
+                isEqual = true;
+                break;
+            }
+
+        }
+
+        console.log(printLine)
+        if (isEqual) {
+            ;
+            break;
+        }
+    }
+
+
+
+}
