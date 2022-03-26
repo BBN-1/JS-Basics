30fe9d262f257ae585316c956f328678859b9c70
Martin Neshev <100307424+BBN-1@users.noreply.github.com>
2022-03-26 10:02:13 +0200

    Create Coins.js

---
+++ While Loop - Exercise/Coins.js
@@ -0,0 +1,50 @@
+function solve(input) {
+
+
+    let change = Number(input[0]) * 100;
+    let coins = 0;
+
+    while (change !== 0) {
+
+        if (change >= 200) {
+            coins++;
+            change -= 200;
+        } else if (change >= 100) {
+            coins++;
+            change -= 100;
+        } else if (change >= 50) {
+            coins++;
+            change -= 50;
+
+        } else if (change >= 20) {
+            coins++;
+            change -= 20
+        } else if (change >= 10) {
+            coins++;
+            change -= 10;
+
+        } else if ( change >= 5){
+            coins++;
+            change -=5;
+        }else if ( change >=2) {
+            coins++;
+            change -= 2;
+        }else if ( change >=1){
+            coins++;
+            change-=1;
+        }else {
+            break;
+        }
+
+
+    }
+
+
+
+        console.log(coins)
+
+
+
+
+
+}
